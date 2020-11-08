1c065d2eff46bab3171d5e4f29c52f05d931f2a1
vinicus rodrigues <vinitecnico@gmail.com>
2020-11-08 15:41:06 -0300

    ajust routes and create two components (productList and productDetails)

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
 import history from "../history";
-import { Home } from "../pages";
+import { Home, ProductList, ProductDetails } from "../pages";
 
 const Routes = () => {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={Home}></Route>
-        {/* <Route exact path='/items' component={ProductListView}></Route>
-                <Route exact path='/items/:id' component={ProductView}></Route> */}
+        <Route exact path="/items" component={ProductList}></Route>
+        <Route exact path="/items/:id" component={ProductDetails}></Route>
         <Redirect from="*" to="/" />
       </Switch>
     </Router>
